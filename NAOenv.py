595a7a783a2385d7853830ba8dc239921e5ee4d1
Felipe de Jesús Félix Arredondo <a00833150@tec.mx>
2024-02-26 20:47:14 -0600

    Cambio de parametros

--- NAOenv.py
+++ NAOenv.py
@@ -39,15 +39,22 @@ class NaoBrain(nn.Module):
                  input_size: int = 2,
                  output_size: int = 2,
                  hidden_size: int = 10,
+                 dropout: float = 0.5
                  ):
         super(NaoBrain, self).__init__()
         self.fc1 = nn.Linear(input_size, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, hidden_size)
         self.fc_mu = nn.Linear(hidden_size, output_size)
+        self.dropout = nn.Dropout(dropout)
         self.fc_sigma = nn.Linear(hidden_size, 1)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = F.relu(self.fc2(x))
+        x = self.dropout(x)
         mu = self.fc_mu(x)
+        mu = torch.tanh(mu)
         sigma = self.fc_sigma(x)
         sigma = torch.exp(sigma)
         return Normal(mu, sigma)
@@ -56,8 +63,8 @@ class NaoEnv(gym.Env):
 
     # [Constructor]{@nao}[Environment Configuration
     def __init__(self, 
-                delta=10,
-                minimum_position=.45,
+                delta=100,
+                minimum_position=.30,
                 with_ball=True,
                 render_mode=False
                 ):
@@ -71,13 +78,13 @@ class NaoEnv(gym.Env):
             p.setRealTimeSimulation(0)
         else:
             self.physicsClient = p.connect(p.DIRECT)
-        p.setGravity(0, 0, -18.81)
+        p.setGravity(0, 0, -9.81)
         
         p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
         # [PyBullet Configuration]{@nao}[URDFs]
         self.planeId = p.loadURDF("plane.urdf")
-        self.naoId = p.loadURDF("Models/humanoid/nao.urdf", [0, 0, .35])
+        self.naoId = p.loadURDF("Models/humanoid/nao.urdf", [0, 0, .33])
         if with_ball:
             self.ballId = p.loadURDF("Models/ball.urdf", [0, 0, 5])
 
@@ -92,7 +99,7 @@ class NaoEnv(gym.Env):
         self.jointDelta = delta
         self.last_launch_time = time.time()
         self.action_space = gym.spaces.Box(low=-delta, high=delta, shape=(p.getNumJoints(self.naoId),))
-        self.observation_space = gym.spaces.Box(low=-np.inf, high=np.inf, shape=(p.getNumJoints(self.naoId),))
+        self.observation_space = gym.spaces.Box(low=-np.inf, high=np.inf, shape=(p.getNumJoints(self.naoId)*2,))
         self.reward_range = (0, 1)
 
         # [Environment Control Variables][@gym][Step]
@@ -103,8 +110,8 @@ class NaoEnv(gym.Env):
         # [PyBullet Configuration]{@nao}[Neck Position]
         neck_position = self._get_neck_position(self.naoId)
         # [PyBullet Configuration]{@nao}[Done]
-        trunc = self._get_done(neck_position, threshold=self.minimum_position * .6)
-        done = self._get_done(neck_position, threshold=self.minimum_position * .6)
+        trunc = self._get_done(neck_position, threshold=self.minimum_position * .3)
+        done = self._get_done(neck_position, threshold=self.minimum_position * .2)
         # [PyBullet Configuration]{@nao}[Reward]
         reward = self._calculate_reward(neck_position,threshold=self.minimum_position)
         # [PyBullet Configuration]{@nao}[Launch Ball]
@@ -140,12 +147,14 @@ class NaoEnv(gym.Env):
         jointStates = []
         for i in range(p.getNumJoints(self.naoId)):
             jointStates.append(p.getJointState(self.naoId, i)[0])
+            jointStates.append(p.getJointState(self.naoId, i)[1])
+
         return torch.tensor(jointStates)
     
     # [Methods]{@nao}[calculate_reward]
     def _calculate_reward(self, neck_position, threshold=0.45):
         if neck_position > threshold:
-            return 1
+            return .3
         else:
             return -1
         
@@ -168,7 +177,8 @@ class NaoEnv(gym.Env):
         for i in range(p.getNumJoints(self.naoId)):
             # Consider the jointLimits
             #p.setJointMotorControl2(self.naoId, i, p.POSITION_CONTROL, actions[i].item()*self.jointDelta)
-            p.setJointMotorControl2(self.naoId, i, p.VELOCITY_CONTROL, targetVelocity=actions[i].item()*self.jointDelta*10, force=100)
+           
+            p.setJointMotorControl2(self.naoId, i, p.VELOCITY_CONTROL, targetVelocity=actions[i].item()*self.jointDelta, force=1000)
     
     def reset(self, seed=None):
         # [PyBullet Configuration]{@nao}[Reset Simulation]
@@ -197,7 +207,7 @@ class NaoEnv(gym.Env):
 
 
 def get_neck_position(naoId):
-    neck_position, _, _, _, _, _ = p.getLinkState(naoId, 9, computeForwardKinematics=True)
+    neck_position, _, _, _, _, _ = p.getLinkState(naoId, 9)
     return neck_position[2]
 
 
@@ -214,19 +224,18 @@ def launch_ball(ballId, start_position,target_position):
     # Change position to a random position
 
 if __name__ == "__main__":
-    env = NaoEnv(render_mode=False,delta=.5)
+    env = NaoEnv(render_mode="human",delta=1.5)
     # Create a NaoBrain with the number of joints
     brain = NaoBrain(env.observation_space.shape[0], env.action_space.shape[0])
-    
-    jointDelta = 0.5
-
     # [Start Simulation]
     state, _ = env.reset()
     # [Start Simulation]{@nao}[Loop]
-    for _ in range(1000):
+    for _ in range(2000):
         # [PyBullet Configuration]{@nao}[Get action]
         dist = brain(state)
         action = dist.sample()
+        ones = torch.ones_like(action)
+        #action = ones
         # [PyBullet Configuration]{@nao}[Add action to the joints]
         state, reward, done, trunc, _ = env.step(action)
         # [PyBullet Configuration]{@nao}[Print]
@@ -234,6 +243,7 @@ if __name__ == "__main__":
         if env._get_collision():
             print ("Collision")
         #print(f"Neck Position: {get_neck_position(env.naoId)}, Reward: {reward}")
+        time.sleep(1./200.)
         if done:
             print ("Done")
             state,_=env.reset()
